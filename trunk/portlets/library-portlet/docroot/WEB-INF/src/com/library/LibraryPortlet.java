9587589e41635f4806f08272480075c61c8051b0
hasan@mpowerglobal.com <hasan@mpowerglobal.com@d5ad669e-e306-cd38-0a76-2f5c367e97eb>
2011-01-18 03:48:22 +0000

    integrating with service layer

--- trunk/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
+++ trunk/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
@@ -1,11 +1,17 @@
 package com.library;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
 
+import com.library.slayer.model.LMSBook;
+import com.library.slayer.model.impl.LMSBookImpl;
+import com.library.slayer.service.LMSBookLocalServiceUtil;
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
@@ -20,7 +26,32 @@ public class LibraryPortlet extends MVCPortlet {
 		String bookTitle = ParamUtil.getString(actionRequest, "bookTitle");
 		String author = ParamUtil.getString(actionRequest, "author");
 		
-		System.out.println("Your inputs ==> " + bookTitle + ", " + author);
+		
+		LMSBook book = new LMSBookImpl();
+		
+		// set primary key
+		long bookId = 0L;
+		try {
+			bookId = 
+				CounterLocalServiceUtil.increment(
+						this.getClass().getName());
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		book.setBookId(bookId);
+		
+		// set UI fields
+		book.setBookTitle(bookTitle);
+		book.setAuthor(author);
+		
+		// set audit field(s)
+		book.setDateAdded(new Date());
+		
+		// insert the book using persistence api
+		try {
+			LMSBookLocalServiceUtil.addLMSBook(book);
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
